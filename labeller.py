67de65dc8674c935dbe7fbedf8b4aa083c065ee0
gkswamy98 <gkswamy98@gmail.com>
2016-10-10 15:51:28 -0700

    Finished implementation of labelling.

--- labeller.py
+++ labeller.py
@@ -1,65 +1,79 @@
-#!usr/bin/env python
+#labeller.py
+from pymongo import MongoClient
 
-def printEmail(collection):
-	data = collection.find().limit(1)[0]
-	print("----------------------------- Email Body -----------------------------")
-	print('\n')
-	print(data['body'])
-	print('\n')
+def get_first_email(collection):
+	"""Gets relevant values of first email in a collection"""
+	email = collection.find().limit(1)[0]
+	relevant_values = ['subject', 'from', 'to', 'body']
+	return {val: email[val] for val in relevant_values}
+
+def get_first_email_id(collection):
+	"""Gets id of first email in a collection"""
+	email = collection.find().limit(1)[0]
+	return email['_id']
+
+def print_email(email):
+	"""Prints given email dictionary"""
+	print("----------------------------- Email ----------------------------------")
+	print(email['subject'])
+	print('FROM: ' + email['from'] + ' TO: ' + email['to'])
+	print('')
+	print(email['body'])
 	print("----------------------------------------------------------------------")
 
-def getLabel(labels):
-	"""Queries user for structured label"""
-	for i in range(len(labels)):
-		print("[" + str(i) + "]" + str(labels[i]))
+def get_label(labels):
+	"""Queries user for label"""
+	for i, elem in enumerate(labels):
+		print("[" + str(i) + "] " + elem
 	print('')
-	label = input("Pick a label: ")
-	if label not in VALID_LABELS:
+	index = input("Pick a number: ")
+	while index < 0 or index >= len(labels):
 		print("That is not a valid option.")
-	if label == 's':
-		return -1
-	else:
-		return str(label)
+		index = input("Pick a number: ")
+	return labels[index]
 
-def getFeatures(label, features):
-	"""Further queries user for extra features"""
-	pass
-
-def mergeDict(a, b):
-	"""Helper function to merge the initial database features
-	with auxilliary features"""
-	pass
-
+def get_features(label, features):
+	"""Queries user for features associated with a given label"""
+	feature_responses = {}
+	for feature in features[label]:
+		response = input(feature + ' ')
+		feature_responses[feature] = response
+	return feature_responses
 
 if __name__ == "__main__":
-	from pymongo import MongoClient
+	#Instantiate connection to DB
 	client = MongoClient('localhost:27017')
 	db = client.emails
-
-	LABELS = ['Moving Event', 'Pet Adoption', 'Attending College', 'Tuition Event', 'Job/Internship Event', \
-			'Medical Event', 'Wedding', 'Funeral', 'Baby', 'Graduation', 'Travel Event', 'College/Scholarship Applications']
-	VALID_LABELS = [str(i) for i in range(len(LABELS))] + ['s']
-	
-	AUX_FEATURES = [['Where was the move?', 'When was the move?'],\
-				['What kind of pet?'],\
-				['Which family member went to college?', 'Which college did they go to?'],\
-				['What was the cost of tuition?', 'Was there any financial aid?'],\
-				['Which company was involved?', 'How long is the internship? (Put \'N/A\' if not internship)', 'Where is the job?'],\
-				['Was it an injury\'[0]\' or illness\'[1]\'', 'Was there a hospitalization? (1 or 0)', 'How long did it last?', 'What was the severity?'],\
-				['Who was involved with the wedding?', 'When was the wedding?'],\
-				['Who was involved with the funeral?', 'When was the funeral?'],\
-				['When did this occur?'],\
-				['When was the graduation?', 'Where did you graduate from?'],\
-				['Where did you travel to?', 'How long was the travel?', 'How much was the travel costs?', 'Was it business or personal? (1 or 0)'],\
-				['Which college/scholarship was the acceptance from?', 'What is the acceptance status?']]
-
-	DBS = [db.moving, db.pet, db.college, db.job, db.medical, db.wedding, db.funeral, db.baby, db.grad, db.travel, db.application]
-
-
-
-	
-
-	numEmailsRead = 0
-
-	printEmail(db.unlabeled)
-	getLabel(LABELS)
+	#Declare constants
+	LABELS = ['Moving Event', 'Pet Adoption', 'Attending College', 'Tuition Event',
+	 		  'Job/Internship Event', 'Medical Event', 'Wedding', 'Funeral',
+			  'Baby', 'Graduation', 'Travel Event', 'College/Scholarship Applications']
+	FEATURES_ARR = [['Where was the move?', 'When was the move?'],
+					['What kind of pet?'],
+					['Which family member went to college?', 'Which college did they go to?'],
+					['What was the cost of tuition?', 'Was there any financial aid?'],
+					['Which company was involved?', 'How long is the internship? (Put \'N/A\' if not internship)', 'Where is the job?'],
+					['Was it an injury\'[0]\' or illness\'[1]\'', 'Was there a hospitalization? (1 or 0)', 'How long did it last?', 'What was the severity?'],
+					['Who was involved with the wedding?', 'When was the wedding?'],
+					['Who was involved with the funeral?', 'When was the funeral?'],
+					['When did this occur?'],
+					['When was the graduation?', 'Where did you graduate from?'],
+					['Where did you travel to?', 'How long was the travel?', 'How much was the travel costs?', 'Was it business or personal? (1 or 0)'],
+					['Which college/scholarship was the acceptance from?', 'What is the acceptance status?']]
+	DBS_ARR = [db.moving, db.pet, db.college, db.job, db.tuition, db.medical,
+			   db.wedding, db.funeral, db.baby, db.grad, db.travel, db.application]
+	assert (len(FEATURES_ARR) == len(LABELS) and len(DBS_ARR) == len(LABELS)), 'Missing entry in one or more of the constant arrays.'
+	#Generate dictionaries
+	FEATURES = {LABELS(i): FEATURES_ARR(i) for i in range(len(LABELS))}
+	DBS = {LABELS(i): DBS_ARR(i) for i in range(len(LABELS))}
+	#Iterate and classify emails
+	unlabeled = db.unlabeled
+	while unlabeled.count != 0:
+		email = get_first_email(unlabeled)
+		print_email(email)
+		label = get_label(LABELS)
+		features = get_features(label, FEATURES)
+		email.update(features)
+		DBS[label].insert_one(email)
+		email_id = get_first_email_id(unlabeled)
+		unlabeled.delete_one({'_id': email_id})
