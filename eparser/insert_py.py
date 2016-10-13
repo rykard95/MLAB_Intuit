45429efbcef43053dff37899458ce4f84f8e7efc
mtrepte <matthewtrepte@gmail.com>
2016-10-12 22:49:29 -0700

    Fixed authentication error

--- eparser/insert_py.py
+++ eparser/insert_py.py
@@ -4,9 +4,9 @@ USERNAME = 'mlabintuit'
 PASSWORD = 'mlab;123'
 MONGODB_URI = 'mongodb://%s:%s@ds048319.mlab.com:48319' % (USERNAME, PASSWORD)
 
-client = MongoClient(MONGODB_URI)
-
-db = client.emails
+connection = MongoClient("ds048319.mlab.com", 48319)
+db = connection['emails']
+db.authenticate(USERNAME, PASSWORD)
 
 def add_email(kvstore):
 	db.unlabeled.insert_one(kvstore)
