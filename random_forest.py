a81837b6e060854b712c2c577ffa99c5b26d6ad0
Gokul Swamy <gokul.swamy@berkeley.edu>
2016-11-21 10:12:44 -0800

    Finished hyperparameter tuning and added in results file.

--- random_forest.py
+++ random_forest.py
@@ -3,35 +3,35 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import linear_model
 import random
 import utils
+from math import sqrt
 
 if __name__ == '__main__':
+    emails = []
+    avg = 0
+    db = utils.get_local_db()
+    for collection in db.collection_names():
+        for record in db.get_collection(collection).find():
+            emails.append([collection] + [record['Text']])
+    random.shuffle(emails)
+    percent_training = .80
+    training_set = emails[:int(percent_training * len(emails))]
+    testing_set = emails[int(percent_training * len(emails)):]
+    training_labels = [row[0] for row in training_set]
+    training_data = [row[1] for row in training_set]
+    testing_labels = [row[0] for row in testing_set]
+    testing_data = [row[1] for row in testing_set]
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(training_data)
+    X = X.toarray()
     num_samples = 100
-    max_estimators = 1000
-    for i in range(1, max_estimators+1):
-        avg = 0
-        for j in range(num_samples):
-            emails = []
-            db = utils.get_local_db()
-            for collection in db.collection_names():
-                for record in db.get_collection(collection).find():
-                    emails.append([collection] + [record['Text']])
-            random.shuffle(emails)
-            percent_training = .80
-            training_set = emails[:int(percent_training * len(emails))]
-            testing_set = emails[int(percent_training * len(emails)):]
-            training_labels = [row[0] for row in training_set]
-            training_data = [row[1] for row in training_set]
-            testing_labels = [row[0] for row in testing_set]
-            testing_data = [row[1] for row in testing_set]
-            vectorizer = TfidfVectorizer()
-            X = vectorizer.fit_transform(training_data)
-            forest = RandomForestClassifier(n_estimators = i) #See http://www.iro.umontreal.ca/~nie/IFT6255/class-email.pdf
-            forest.fit(X.toarray(), training_labels)
-            vectorized_testing_data = [vectorizer.transform([email]) for email in testing_data]
-            total = len(vectorized_testing_data)
-            correct = 0
-            for k in range(total):
-                if forest.predict(vectorized_testing_data[k]) == testing_labels[k]:
-                    correct += 1
-            avg += (correct/total)
-        print("i: "+str(i)+", %: "+str((avg/num_samples)))
+    for i in range(num_samples):
+        forest = RandomForestClassifier(n_estimators = int(sqrt(len(X[0]))))
+        forest.fit(X, training_labels)
+        vectorized_testing_data = [vectorizer.transform([email]) for email in testing_data]
+        total = len(vectorized_testing_data)
+        correct = 0
+        for k in range(total):
+            if forest.predict(vectorized_testing_data[k]) == testing_labels[k]:
+                correct += 1
+        avg += (correct/total)
+    print("trees: "+str(i)+", %: "+str((avg/num_samples)))
