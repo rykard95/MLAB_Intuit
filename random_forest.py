14ff45fa48e7ac3a1941af786d81fc2634192b7d
Gokul Swamy <gokul.swamy@berkeley.edu>
2016-11-20 19:19:55 -0800

    Updated hyperparameter tuning

--- random_forest.py
+++ random_forest.py
@@ -7,44 +7,31 @@ import utils
 if __name__ == '__main__':
     num_samples = 100
     max_estimators = 1000
-    max_features = 100
-    max_average = [0, 0, 0]
-    for i in range(max_estimators):
-        if i%10 == 0:
-            print(str(i/10)+"%")
-        for j in range(max_features):
-            avg = 0
-            for k in range(num_samples):
-                emails = []
-                db = utils.get_local_db()
-                for collection in db.collection_names():
-                    for record in db.get_collection(collection).find():
-                        emails.append([collection] + [record['Text']])
-                random.shuffle(emails)
-                percent_training = .80
-                training_set = emails[:int(percent_training * len(emails))]
-                testing_set = emails[int(percent_training * len(emails)):]
-                training_labels = [row[0] for row in training_set]
-                training_data = [row[1] for row in training_set]
-                testing_labels = [row[0] for row in testing_set]
-                testing_data = [row[1] for row in testing_set]
-                vectorizer = TfidfVectorizer()
-                X = vectorizer.fit_transform(training_data)
-                forest = RandomForestClassifier(n_estimators = 10, max_features = 9) #See http://www.iro.umontreal.ca/~nie/IFT6255/class-email.pdf
-                forest.fit(X.toarray(), training_labels)
-                vectorized_testing_data = [vectorizer.transform([email]) for email in testing_data]
-                total = len(vectorized_testing_data)
-                correct = 0
-                for a in range(total):
-                    if forest.predict(vectorized_testing_data[a]) == testing_labels[a]:
-                        correct += 1
-                avg += (correct/total)
-            if avg/num_samples > max_average[2]:
-                max_average = [i, j, avg/num_samples]
-
-    print("best estimator:")
-    print(max_average[0])
-    print("best features:")
-    print(max_average[1])
-    print("best average:")
-    print(max_average[2])
+    for i in range(1, max_estimators+1):
+        avg = 0
+        for j in range(num_samples):
+            emails = []
+            db = utils.get_local_db()
+            for collection in db.collection_names():
+                for record in db.get_collection(collection).find():
+                    emails.append([collection] + [record['Text']])
+            random.shuffle(emails)
+            percent_training = .80
+            training_set = emails[:int(percent_training * len(emails))]
+            testing_set = emails[int(percent_training * len(emails)):]
+            training_labels = [row[0] for row in training_set]
+            training_data = [row[1] for row in training_set]
+            testing_labels = [row[0] for row in testing_set]
+            testing_data = [row[1] for row in testing_set]
+            vectorizer = TfidfVectorizer()
+            X = vectorizer.fit_transform(training_data)
+            forest = RandomForestClassifier(n_estimators = i) #See http://www.iro.umontreal.ca/~nie/IFT6255/class-email.pdf
+            forest.fit(X.toarray(), training_labels)
+            vectorized_testing_data = [vectorizer.transform([email]) for email in testing_data]
+            total = len(vectorized_testing_data)
+            correct = 0
+            for k in range(total):
+                if forest.predict(vectorized_testing_data[k]) == testing_labels[k]:
+                    correct += 1
+            avg += (correct/total)
+        print("i: "+str(i)+", %: "+str((avg/num_samples)))
