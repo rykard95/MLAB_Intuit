5081422b528ee4ed10f64691f6398d812ab3dd75
Gokul Swamy <gokul.swamy@berkeley.edu>
2016-11-29 12:25:16 -0800

    Updated confusion matrix code to use testing set

--- random_forest_confusion_matrix.py
+++ random_forest_confusion_matrix.py
@@ -9,47 +9,41 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import linear_model
 from sklearn.metrics import confusion_matrix
 from math import sqrt
+from pickle import load
 
 if __name__ == '__main__':
-    percent_training = .30 # proportion of data to use for training
-
-    # get emails and labels from local mongodb
+    # get training corpus/labels
     emails = []
     label_set = []
     db = utils.get_local_db()
     for collection in db.collection_names():
-        label_set.append(collection)
+        # the [:-6] is to account for the suffix '_clean'
+        label_set.append(collection[:-6])
         for record in db.get_collection(collection).find():
             emails.append([collection] + [record['Text']])
 
-    # shuffle and split emails
-    random.shuffle(emails)
-    training_set = emails[:int(percent_training * len(emails))]
-    testing_set = emails[int(percent_training * len(emails)):]
-    training_labels = [row[0] for row in training_set]
-    training_data = [row[1] for row in training_set]
-    testing_labels = [row[0] for row in testing_set]
-    testing_data = [row[1] for row in testing_set]
-
-    # tf-idf vectorize training set
+    # vectorize corpus
+    labels = [row[0][:-6] for row in emails]
+    data = [row[1] for row in emails]
     vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(training_data)
+    X = vectorizer.fit_transform(data)
     X = X.toarray()
 
-    # tf-idf vectorize testing set
-    vectorized_testing_data = [vectorizer.transform([email]) for email in testing_data]
-    total = len(vectorized_testing_data)
-
-    # create random forest and compute predictions
+    # create random forest
     forest = RandomForestClassifier(n_estimators = int(sqrt(len(X[0])))+1)
-    forest.fit(X, training_labels)
-    predictions = []
-    for i in range(len(vectorized_testing_data)):
-        predictions.append(forest.predict(vectorized_testing_data[i]))
+    forest.fit(X, labels)
+
+    # read in and vectorize testing data
+    with open('intuit_data', 'rb') as f:
+        data = load(f)
+    email_vectors = [vectorizer.transform([email['Text']]) for email in data['data']]
+    email_labels = [label if label != 'negative' else 'negatives' for label in data['label']] # if-else to account for variation in naming
+
+    # predict classification
+    predicted_email_labels = [forest.predict(email_vector)[0] for email_vector in email_vectors]
 
     # generate confusion matrix: C_i,j is equal to the number of observations known to be in group i but predicted to be in group j
-    cm = confusion_matrix(predictions, testing_labels, labels=label_set)
-    label_set = [label[:-6] for label in label_set]
+    cm = confusion_matrix(predicted_email_labels, email_labels, labels=label_set)
     df_cm = pd.DataFrame(cm, index=label_set, columns=label_set)
     plt.figure(figsize = (10,7))
     sn.heatmap(df_cm, annot=True)
